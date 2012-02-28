a44ba2ea0d402a546a615cb7a1f6dc9c89db7ca1
Markus Dahm <markus.dahm@akquinet.de>
2012-02-28 15:16:11 +0100

    fixed mapping

--- jbpm-5.1.0.Final/jbpm-human-task-noPersistenceXml/src/main/java/org/jbpm/task/Deadline.java
+++ jbpm-5.1.0.Final/jbpm-human-task-noPersistenceXml/src/main/java/org/jbpm/task/Deadline.java
@@ -33,136 +33,136 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 import org.jbpm.task.utils.CollectionUtils;
 
 @Entity
 @Table(name = "JBPM_DEADLINE")
 public class Deadline implements Externalizable {
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private long id;
 
-	@OneToMany(cascade = CascadeType.ALL)
-	@JoinColumn(name = "Deadline_Documentation_Id", nullable = true)
-	private List<I18NText> documentation = Collections.emptyList();
+  @OneToMany(cascade = CascadeType.ALL)
+  @JoinColumn(name = "Deadline_Documentation_Id", nullable = true)
+  private List<I18NText> documentation = Collections.emptyList();
 
-	@Column(name = "deadline_date")
-	private Date date;
+  @Temporal(TemporalType.DATE)
+  @Column(name = "deadline_date")
+  private Date date;
 
-	@OneToMany(cascade = CascadeType.ALL)
-	@JoinColumn(name = "Deadline_Escalation_Id", nullable = true)
-	private List<Escalation> escalations = Collections.emptyList();
+  @OneToMany(cascade = CascadeType.ALL)
+  @JoinColumn(name = "Deadline_Escalation_Id", nullable = true)
+  private List<Escalation> escalations = Collections.emptyList();
 
-	private boolean escalated;
+  private boolean escalated;
 
-	public void writeExternal(final ObjectOutput out) throws IOException {
-		out.writeLong(id);
+  public void writeExternal(final ObjectOutput out) throws IOException {
+    out.writeLong(id);
 
-		if (date != null) {
-			out.writeBoolean(true);
-			out.writeLong(date.getTime());
-		} else {
-			out.writeBoolean(false);
-		}
-		CollectionUtils.writeI18NTextList(documentation, out);
-		CollectionUtils.writeEscalationList(escalations, out);
+    if (date != null) {
+      out.writeBoolean(true);
+      out.writeLong(date.getTime());
+    } else {
+      out.writeBoolean(false);
+    }
+    CollectionUtils.writeI18NTextList(documentation, out);
+    CollectionUtils.writeEscalationList(escalations, out);
 
-		out.writeBoolean(escalated);
-	}
+    out.writeBoolean(escalated);
+  }
 
-	public void readExternal(final ObjectInput in) throws IOException,
-			ClassNotFoundException {
-		id = in.readLong();
+  public void readExternal(final ObjectInput in) throws IOException, ClassNotFoundException {
+    id = in.readLong();
 
-		if (in.readBoolean()) {
-			date = new Date(in.readLong());
-		}
-		documentation = CollectionUtils.readI18NTextList(in);
-		escalations = CollectionUtils.readEscalationList(in);
+    if (in.readBoolean()) {
+      date = new Date(in.readLong());
+    }
+    documentation = CollectionUtils.readI18NTextList(in);
+    escalations = CollectionUtils.readEscalationList(in);
 
-		escalated = in.readBoolean();
-	}
+    escalated = in.readBoolean();
+  }
 
-	public long getId() {
-		return id;
-	}
+  public long getId() {
+    return id;
+  }
 
-	public void setId(final long id) {
-		this.id = id;
-	}
+  public void setId(final long id) {
+    this.id = id;
+  }
 
-	public List<I18NText> getDocumentation() {
-		return documentation;
-	}
+  public List<I18NText> getDocumentation() {
+    return documentation;
+  }
 
-	public void setDocumentation(final List<I18NText> documentation) {
-		this.documentation = documentation;
-	}
+  public void setDocumentation(final List<I18NText> documentation) {
+    this.documentation = documentation;
+  }
 
-	public Date getDate() {
-		return date;
-	}
+  public Date getDate() {
+    return date;
+  }
 
-	public void setDate(final Date date) {
-		this.date = date;
-	}
+  public void setDate(final Date date) {
+    this.date = date;
+  }
 
-	public List<Escalation> getEscalations() {
-		return escalations;
-	}
+  public List<Escalation> getEscalations() {
+    return escalations;
+  }
 
-	public void setEscalations(final List<Escalation> escalations) {
-		this.escalations = escalations;
-	}
+  public void setEscalations(final List<Escalation> escalations) {
+    this.escalations = escalations;
+  }
 
-	public boolean isEscalated() {
-		return escalated;
-	}
+  public boolean isEscalated() {
+    return escalated;
+  }
 
-	public void setEscalated(final boolean escalated) {
-		this.escalated = escalated;
-	}
+  public void setEscalated(final boolean escalated) {
+    this.escalated = escalated;
+  }
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((date == null) ? 0 : date.hashCode());
-		result = prime * result + CollectionUtils.hashCode(documentation);
-		result = prime * result + CollectionUtils.hashCode(escalations);
-		result = prime * result + (escalated ? 1231 : 1237);
-		result = prime * result + (int) (id ^ (id >>> 32));
-		return result;
-	}
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((date == null) ? 0 : date.hashCode());
+    result = prime * result + CollectionUtils.hashCode(documentation);
+    result = prime * result + CollectionUtils.hashCode(escalations);
+    result = prime * result + (escalated ? 1231 : 1237);
+    result = prime * result + (int) (id ^ (id >>> 32));
+    return result;
+  }
 
-	@Override
-	public boolean equals(final Object obj) {
-		if (this == obj) {
-			return true;
-		}
-		if (obj == null) {
-			return false;
-		}
-		if (!(obj instanceof Deadline)) {
-			return false;
-		}
-		final Deadline other = (Deadline) obj;
+  @Override
+  public boolean equals(final Object obj) {
+    if (this == obj) {
+      return true;
+    }
+    if (obj == null) {
+      return false;
+    }
+    if (!(obj instanceof Deadline)) {
+      return false;
+    }
+    final Deadline other = (Deadline) obj;
 
-		if (date == null) {
-			if (other.date != null) {
-				return false;
-			}
-		} else if (date.getTime() != other.date.getTime()) {
-			return false;
-		}
+    if (date == null) {
+      if (other.date != null) {
+        return false;
+      }
+    } else if (date.getTime() != other.date.getTime()) {
+      return false;
+    }
 
-		if (escalated != other.escalated) {
-			return false;
-		}
+    if (escalated != other.escalated) {
+      return false;
+    }
 
-		return CollectionUtils.equals(documentation, other.documentation)
-				&& CollectionUtils.equals(escalations, other.escalations);
-	}
-
+    return CollectionUtils.equals(documentation, other.documentation) && CollectionUtils.equals(escalations, other.escalations);
+  }
 }
